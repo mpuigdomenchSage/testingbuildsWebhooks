e0bdc1a55b88b31575916463550e59f8e15b3588
mpuigdomenech <miquel.puigdomenech@sage.com>
2019-10-14 11:27:13 +0200

    project A

---
+++ src/projectA/ClassLibraryA/Frier.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibraryA
+{
+    public class Frier
+    {
+        public static IEnumerable<string> DoFry(int nuget) =>
+            Enumerable.Range(1, nuget).Select(i => $"frying nuget number {i} of {nuget}");            
+    }
+}
---
+++ src/projectA/ConsoleAppA/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibraryA;
+using System;
+
+namespace ConsoleAppA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of nugets");
+
+            var n = Console.Read();
+            var fN = Frier.DoFry(n);
+            foreach (var item in fN)
+                Console.WriteLine(item);
+        }
+    }
+}
