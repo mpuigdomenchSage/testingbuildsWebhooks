b40705436be5cc8b34886a6abe682cbaf3dbefe7
mpuigdomenech <miquel.puigdomenech@sage.com>
2019-10-14 11:37:42 +0200

    project B

---
+++ src/projectB/ClassLibraryBB/Pan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassLibraryBB
+{
+    public class Pan
+    {
+            public static IEnumerable<string> DoPutInThaPan(int steak) =>
+                Enumerable.Range(1, steak).Select(i => $" {i}  steack in the pan , of {steak}");
+    }
+}
---
+++ src/projectB/ConsoleAppB/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibraryBB;
+using System;
+
+namespace ConsoleAppB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of steaks");
+
+            var n = Console.Read();
+            var fN = Pan.DoPutInThaPan(n);
+            foreach (var item in fN)
+                Console.WriteLine(item);
+        }
+    }
+}
