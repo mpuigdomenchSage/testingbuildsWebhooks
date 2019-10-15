df7cb3b983eb988662e6c15f407c3b066c32bdea
mpuigdomenech <miquel.puigdomenech@sage.com>
2019-10-15 10:51:07 +0200

    onlyB things

--- src/projectB/ConsoleAppB/Program.cs
+++ src/projectB/ConsoleAppB/Program.cs
@@ -13,6 +13,8 @@ namespace ConsoleAppB
             var fN = Pan.DoPutInThaPan(n);
             foreach (var item in fN)
                 Console.WriteLine(item);
+            foreach (var item in fN)
+                Console.WriteLine(item);
         }
     }
 }
