271ab5ebb25cbd22e561175e9aa79cc0259e0752
mpuigdomenech <miquel.puigdomenech@sage.com>
2019-10-25 14:20:23 +0200

    sdf

--- src/projectA/ConsoleAppA/Program.cs
+++ src/projectA/ConsoleAppA/Program.cs
@@ -21,6 +21,8 @@ namespace ConsoleAppA
                 Console.WriteLine(item);
             foreach (var item in fN)
                 Console.WriteLine(item);
+            foreach (var item in fN)
+                Console.WriteLine(item);
         }
     }
 }
